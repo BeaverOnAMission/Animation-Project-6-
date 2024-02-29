b75d50ffe6ba77f4e8bf31f0a942e6215b593af0
BeaverOnAMission <babenko.bohdan01@gmail.com>
2024-02-29 20:05:17 +0200

    Initial Commit

---
+++ Animation(Project 6)/Animation_Project_6_App.swift
@@ -0,0 +1,17 @@
+//
+//  Animation_Project_6_App.swift
+//  Animation(Project 6)
+//
+//  Created by mac on 09.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Animation_Project_6_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
